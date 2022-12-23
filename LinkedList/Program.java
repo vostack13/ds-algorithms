641e07853d42cfa05501b6b121e1963816a0e540
Stanislav <stanislav.vozikov@gmail.com>
2022-12-23 16:42:06 +0300

    feat(Linked List): added template

---
+++ LinkedList/Program.java
@@ -0,0 +1,14 @@
+public class Program
+{
+    public static void main(String args[]) {
+        LinkedList list1 = new LinkedList();
+        list1.addInTail(new Node(1));
+        list1.addInTail(new Node(2));
+        list1.addInTail(new Node(3));
+
+        LinkedList list2 = new LinkedList();
+        list2.addInTail(new Node(4));
+        list2.addInTail(new Node(5));
+        list2.addInTail(new Node(6));
+    }
+}
