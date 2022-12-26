3feb0826e3bdcd7e9ec07e0ab33cea6e589248b3
Stanislav <stanislav.vozikov@gmail.com>
2022-12-27 01:24:16 +0300

    feat(Doubly Linked List): added solution

--- Program.java
+++ Program.java
@@ -23,69 +23,7 @@ public class Program {
         // LinkedListUtils utils = new LinkedListUtils();
         // utils.run(list1, list2);
 
-        // LinkedList2Test test2 = new LinkedList2Test();
-        // test2.run();
-
-    //     System.out.println();
-    //     DynArray array = new DynArray<Integer>(Integer.class);
-
-    //     DynArrayTest dynArrayTest = new DynArrayTest();
-    //     dynArrayTest.run();
-    
-        // StackTest stackTest = new StackTest();
-        // stackTest.run();
-        
-        // StackUtils stackUtils = new StackUtils();
-        // System.out.println("1: " + stackUtils.getIsBalanced("())("));
-        // System.out.println("2: " + stackUtils.getIsBalanced("))(("));
-        // System.out.println("3: " + stackUtils.getIsBalanced("((())"));
-        // System.out.println("4: " + stackUtils.getIsBalanced("(()()(()"));
-        // System.out.println("5: " + stackUtils.getIsBalanced("(()((())()))"));
-        
-        // try {
-        //     Double calculatorResult = stackUtils.calculation("8 2 + 5 * 9 + 9 - 70 - 3 / =");
-        //     System.out.println(calculatorResult);
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }
-        
-        // QueueTest queueTest = new QueueTest();
-        // queueTest.run();
-
-        // QueueUtils queueUtils = new QueueUtils();
-        // Queue<Integer> queue = new Queue<Integer>();
-
-        // queue.enqueue(1);
-        // queue.enqueue(2);
-        // queue.enqueue(3);
-        // queue.enqueue(4);
-        // queue.enqueue(5);
-        // // queueUtils.circleRotateQueue(queue);
-        // 
-        // QueueOnStacks<Integer> queueOnStacks = new QueueOnStacks<Integer>();
-        // queueOnStacks.enqueue(1);
-        // queueOnStacks.enqueue(2);
-        // queueOnStacks.enqueue(3);
-        // queueOnStacks.enqueue(4);
-        // queueOnStacks.enqueue(5);
-        // 
-        // System.out.println(queueOnStacks.size());
-        // System.out.println(queueOnStacks.dequeue());
-        // System.out.println(queueOnStacks.size());
-        // System.out.println(queueOnStacks.dequeue());
-        // System.out.println(queueOnStacks.size());
-        // System.out.println(queueOnStacks.dequeue());
-        // System.out.println(queueOnStacks.size());
-        // System.out.println(queueOnStacks.dequeue());
-        // System.out.println(queueOnStacks.size());
-        // System.out.println(queueOnStacks.dequeue());
-        // System.out.println(queueOnStacks.size());
-        // System.out.println(queueOnStacks.dequeue());
-        // System.out.println(queueOnStacks.size());
-        
-        DequeUtils dequeUtils = new DequeUtils();
-        System.out.println(dequeUtils.isPalindrome("banana"));
-        System.out.println(dequeUtils.isPalindrome("banab"));
-        System.out.println(dequeUtils.isPalindrome("b"));
-     }
+        LinkedList2Test test2 = new LinkedList2Test();
+        test2.run();
+    }
 }
