e60ed217de865f4fd747d7c8fadb9940173ab12e
Stanislav <stanislav.vozikov@gmail.com>
2023-01-09 11:22:25 +0300

    fix(Queue) fixed after review

--- QueueUtils.java
+++ QueueUtils.java
@@ -5,14 +5,6 @@ public class QueueUtils {
     public <T> void circleRotateQueue(Queue<T> queue) {
         while (queue.size() > 0) {
             T item = queue.dequeue();
-
-            try {
-                System.out.println(item);
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
             queue.enqueue(item);
         }
     }
