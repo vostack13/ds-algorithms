e0b9dcfc4e112ca2f7d1490ec93728a520ae4f99
Stanislav <stanislav.vozikov@gmail.com>
2023-01-26 14:35:02 +0300

    fix(PowerSet): added solution

--- test/PowerSetTest.java
+++ test/PowerSetTest.java
@@ -11,7 +11,7 @@ public class PowerSetTest {
         PowerSet powerSetC = new PowerSet();
         PowerSet powerSetD = new PowerSet();
 
-        int size = 20000;
+        int size = 200000;
 
         long start = System.currentTimeMillis();
         
